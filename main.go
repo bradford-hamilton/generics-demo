1623c70c6c44dca87350df0993aee1470af8a43e
Bradford Lamson-Scribner <brad.lamson@gmail.com>
2020-09-10 17:22:16 -0600

    update comments pointing to new generics examples. Will re-do this later.

--- main.go
+++ main.go
@@ -1,26 +1,25 @@
 package main
 
-import (
-	"fmt"
-)
+// This is no longer the current draft, here are some new examples to come back to:
+// https://github.com/golang/go/tree/dev.go2go/src/cmd/go2go/testdata/go2path/src
 
-type Stringer interface {
-	String() string	
-}
+// type Stringer interface {
+// 	String() string
+// }
 
-type user struct {
-    name string
-}
+// type user struct {
+//     name string
+// }
 
-func (n *user) String() string {
-	return n.name
-}
+// func (n *user) String() string {
+// 	return n.name
+// }
 
-func toString(type T Stringer)(item T) string {
-	return item.String()
-}
+// func toString(type T Stringer)(item T) string {
+// 	return item.String()
+// }
 
-func main() {
-	s := toString(&user{name: "Bradford"})
-	fmt.Println(s)
-}
+// func main() {
+// 	s := toString(&user{name: "Bradford"})
+// 	fmt.Println(s)
+// }
